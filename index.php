aee5bd1722f2c5e20fb72b1274d30e9182ebac99
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-05-19 13:12:13 +0200

    stuttura

--- index.php
+++ index.php
@@ -3,35 +3,35 @@
 
         [
             'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
+            'description' => 'Hotel Belvedere',
             'parking' => true,
             'vote' => 4,
             'distance_to_center' => 10.4
         ],
         [
             'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
+            'description' => 'Hotel Futuro',
             'parking' => true,
             'vote' => 2,
             'distance_to_center' => 2
         ],
         [
             'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
+            'description' => 'Hotel Rivamare',
             'parking' => false,
             'vote' => 1,
             'distance_to_center' => 1
         ],
         [
             'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
+            'description' => 'Hotel Bellavista',
             'parking' => false,
             'vote' => 5,
             'distance_to_center' => 5.5
         ],
         [
             'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
+            'description' => 'Hotel Milano',
             'parking' => true,
             'vote' => 2,
             'distance_to_center' => 50
@@ -46,9 +46,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>PHP-Hotels</title>
 </head>
 <body>
-    
+    <div class="container">
+        <div class="card">
+            <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Descrizione</th>
+                    <th scope="col">Parcheggio</th>
+                    <th scope="col">Voto</th>
+                    <th scope="col">Distanza dal centro</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        foreach($hotels as $hotel){
+                            echo "<tr>
+                              <th scope='row'>".$hotel['name']."</th>
+                              <td>".$hotel['description']."</td>
+                              <td>".($hotel['parking'] ? 'Si' : 'No')."</td>
+                              <td>".$hotel['vote']."</td>
+                              <td>".$hotel['distance_to_center']." Km"."</td>
+                            </tr>";
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
