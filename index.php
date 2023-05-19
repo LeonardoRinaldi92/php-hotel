81269177946ca4c1048d6407684e0b26cb822ffb
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-05-19 16:19:10 +0200

    completo con bonus e risposta in caso di nullo

--- index.php
+++ index.php
@@ -79,7 +79,7 @@
     <title>PHP-Hotels</title>
 </head>
 <body>
-    <div class="container">
+    <div class="container mt-5">
         <form  method="GET" action="index.php">
             <div class="form-check">
                 <input class="form-check-input" type="checkbox" value="true" name="park">
@@ -95,38 +95,50 @@
                 <option value="4">&#9733;&#9733;&#9733;&#9733;</option>
                 <option value="5">&#9733;&#9733;&#9733;&#9733;&#9733;</option>
             </select>
-            <button type="submit"> vai!</button>
+            <button type="submit" class="mt-2"> vai!</button>
         </form>
-
     </div>
-    <div class="container">
-        <div class="card">
-            <table class="table">
-                <thead>
-                  <tr>
-                    <th scope="col">Nome</th>
-                    <th scope="col">Descrizione</th>
-                    <th scope="col">Parcheggio</th>
-                    <th scope="col">Voto</th>
-                    <th scope="col">Distanza dal centro</th>
-                  </tr>
-                </thead>
-                <tbody>
-                    <?php
-                        foreach($hotels as $hotel){
-                            echo "<tr>
-                              <th scope='row'>".$hotel['name']."</th>
-                              <td>".$hotel['description']."</td>
-                              <td>".($hotel['parking'] ? 'Si' : 'No')."</td>
-                              <td>".$hotel['vote']."</td>
-                              <td>".$hotel['distance_to_center']." Km"."</td>
-                            </tr>";
-                        }
-                    ?>
-                </tbody>
+    <?php
+        if($hotels) {
+            echo "<div class='container mt-5'>
+                        <div class='card'>
+                            <table class='table'>
+                                <thead>
+                                <tr>
+                                    <th scope='col'>Nome</th>
+                                    <th scope='col'>Descrizione</th>
+                                    <th scope='col'>Parcheggio</th>
+                                    <th scope='col'>Voto</th>
+                                    <th scope='col'>Distanza dal centro</th>
+                                </tr>
+                                </thead>
+                                <tbody>";
+        };
+        foreach($hotels as $hotel){
+            echo "<tr>
+            <th scope='row'>".$hotel['name']."</th>
+            <td>".$hotel['description']."</td>
+            <td>".($hotel['parking'] ? 'Si' : 'No')."</td>
+            <td>".$hotel['vote']."</td>
+            <td>".$hotel['distance_to_center']." Km"."</td>
+            </tr>";
+        }
+        if($hotels) {
+            echo "</tbody>
             </table>
         </div>
-    </div>
+    </div>";
+        }
+
+
+        if(!$hotels) {
+            echo 
+            "<div class='container mt-4'>
+                <p>Nessun Risultato</p>
+            </div>";
+
+        }
+    ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
